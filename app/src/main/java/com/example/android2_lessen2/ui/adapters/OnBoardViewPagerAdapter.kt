1640e8fb130f1b270c4aef8508c3599e3e75c638
Jafar Kasymov <116582499+JafarKasimov@users.noreply.github.com>
2023-02-04 16:34:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android2_lessen2/ui/adapters/OnBoardViewPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.android2_lessen2.ui.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.android2_lessen2.ui.fragments.onboard.OnBoardPagingFragment
+
+class OnBoardViewPagerAdapter (fragment: Fragment): FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment = OnBoardPagingFragment().apply{
+    arguments = Bundle().apply {
+     putInt(OnBoardPagingFragment.ARG_ON_BOARD_PAGE_POSITION,position)
+    }
+    }
+
+}
---
+++ app/src/main/java/com/example/android2_lessen2/ui/fragments/onboard/OnBoardPagingFragment.kt
@@ -0,0 +1,50 @@
+package com.example.android2_lessen2.ui.fragments.onboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android2_lessen2.R
+import com.example.android2_lessen2.databinding.FragmentOnBoardPegingBinding
+
+class OnBoardPagingFragment : Fragment() {
+
+   private lateinit var binding: FragmentOnBoardPegingBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+       binding = FragmentOnBoardPegingBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+    }
+
+    private fun initialize() = with(binding) {
+        when (requireArguments().getInt(ARG_ON_BOARD_PAGE_POSITION)) {
+            0 -> {
+                tvText.text = "Очень удобный функционал"
+                lottie.setAnimation(R.raw.lottie1)
+
+            }
+            1 -> {
+                tvText.text = "Быстрый качественный продукт"
+                lottie.setAnimation(R.raw.lottie2)
+
+            }
+            2 -> {
+                tvText.text = "Куча функций и интересных фишек"
+                lottie.setAnimation(R.raw.lottie3)
+            }
+        }
+    }
+    companion object{
+        const val ARG_ON_BOARD_PAGE_POSITION = "onBoardPager"
+    }
+
+
+}
